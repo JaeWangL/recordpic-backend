0093e8be10885fc17dcf8e440f99443a3fcdca68
JaeWangL <jnso5072@outlook.kr>
2021-02-06 23:15:21 +0900

    [Fix] Fix NotificationModule has no service provider

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,18 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AllCommandHandlers } from './commands';
+import { AllCommandHandlers, AllQueryHandlers } from './commands';
 import { NotificationsController } from './controllers';
 import { NotificationEntity } from './domain';
 import { NotificationService } from './services';
 
+const AllControllers = [NotificationsController];
 const AllEntities = [NotificationEntity];
 const AllServices = [NotificationService];
 
 @Module({
   imports: [CqrsModule, TypeOrmModule.forFeature([...AllEntities])],
-  controllers: [NotificationsController],
-  providers: [...AllCommandHandlers],
+  controllers: [...AllControllers],
+  providers: [...AllServices, ...AllCommandHandlers, ...AllQueryHandlers],
   exports: [...AllServices],
 })
 export default class NotificationsModule {}
