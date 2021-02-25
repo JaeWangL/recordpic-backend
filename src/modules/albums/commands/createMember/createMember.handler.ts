dd780415759bcd5d0acf754cdfd6f69bda0ef76a
JaeWangL <jnso5072@outlook.kr>
2021-02-25 23:49:17 +0900

    [Feature] Add notifications when creating moment and member

--- src/modules/albums/commands/createMember/createMember.handler.ts
+++ src/modules/albums/commands/createMember/createMember.handler.ts
@@ -1,5 +1,8 @@
 import { Logger, NotFoundException } from '@nestjs/common';
 import { ICommandHandler, CommandHandler } from '@nestjs/cqrs';
+import { NotificationType } from '@common/enum-types';
+import { NotificationEntity } from '@notifications/domain';
+import { NotificationService } from '@notifications/services';
 import { MemberEntity } from '../../domain';
 import { CreateMemberRequest, MemberPreviewDto } from '../../dtos';
 import { AlbumService, MemberService } from '../../services';
@@ -8,7 +11,11 @@ import CreateMemberCommand from './createMember.command';
 
 @CommandHandler(CreateMemberCommand)
 export default class CreateMemberHandler implements ICommandHandler<CreateMemberCommand, MemberPreviewDto | undefined> {
-  constructor(private readonly albumSvc: AlbumService, private readonly memberSvc: MemberService) {}
+  constructor(
+    private readonly albumSvc: AlbumService,
+    private readonly memberSvc: MemberService,
+    private readonly notifyingSvc: NotificationService,
+  ) {}
 
   async execute(command: CreateMemberCommand): Promise<MemberPreviewDto | undefined> {
     Logger.log('Create Member...', 'CreateMemberCommand');
@@ -25,10 +32,16 @@ export default class CreateMemberHandler implements ICommandHandler<CreateMember
       }
 
       const newMember = await this.createNewMemberAsync(req, album.id);
+
+      await this.sendNotifications(newMember);
+
       return toMemberPreviewDTO(newMember);
     }
     if (req.albumId) {
       const newMember = await this.createNewMemberAsync(req, req.albumId);
+
+      await this.sendNotifications(newMember);
+
       return toMemberPreviewDTO(newMember);
     }
 
@@ -39,5 +52,19 @@ export default class CreateMemberHandler implements ICommandHandler<CreateMember
     const newMember = new MemberEntity(albumId, req.userId, req.userEmail, req.userName, req.userImageUrl);
 
     return await this.memberSvc.createAsync(newMember);
+  }
+
+  private async sendNotifications(newMember: MemberEntity): Promise<void> {
+    const [allMembers, count] = await this.memberSvc.findByAlbumIdAsync(newMember.albumId);
+    allMembers.forEach(async (m) => {
+      const newNotifying = new NotificationEntity(
+        m.userId,
+        NotificationType.AddMember,
+        newMember.userName,
+        newMember.userImageUrl,
+        newMember.albumId,
+      );
+      await this.notifyingSvc.createAsync(newNotifying);
+    });
   }
 }
--- src/modules/albums/commands/createMoment/createMoment.handler.ts
+++ src/modules/albums/commands/createMoment/createMoment.handler.ts
@@ -1,14 +1,22 @@
 import { Logger } from '@nestjs/common';
 import { ICommandHandler, CommandHandler } from '@nestjs/cqrs';
+import { NotificationType } from '@common/enum-types';
+import { NotificationEntity } from '@notifications/domain';
+import { NotificationService } from '@notifications/services';
 import { MomentEntity, PhotoEntity } from '../../domain';
 import { CreateMomentRequest, MomentPreviewDto } from '../../dtos';
-import { MomentService, PhotoService } from '../../services';
+import { MemberService, MomentService, PhotoService } from '../../services';
 import { toMomentPreviewWithPhotoDTO } from '../moment.extensions';
 import CreateMomentCommand from './createMoment.command';
 
 @CommandHandler(CreateMomentCommand)
 export default class CreateMomentHandler implements ICommandHandler<CreateMomentCommand, MomentPreviewDto> {
-  constructor(private readonly momentSvc: MomentService, private readonly photoSvc: PhotoService) {}
+  constructor(
+    private readonly memberSvc: MemberService,
+    private readonly momentSvc: MomentService,
+    private readonly photoSvc: PhotoService,
+    private readonly notifyingSvc: NotificationService,
+  ) {}
 
   async execute(command: CreateMomentCommand): Promise<MomentPreviewDto> {
     Logger.log('Create Moment...', 'CreateMomentCommand');
@@ -16,6 +24,8 @@ export default class CreateMomentHandler implements ICommandHandler<CreateMoment
 
     const newMoment = await this.createNewMomentAsync(req);
     const newPhotos = await this.createPhotos(req, newMoment.id);
+
+    await this.sendNotifications(req, newMoment.id);
 
     return toMomentPreviewWithPhotoDTO(newMoment, newPhotos[0], newPhotos.length);
   }
@@ -30,5 +40,25 @@ export default class CreateMomentHandler implements ICommandHandler<CreateMoment
     const newPhotos = req.photos.map((p) => new PhotoEntity(req.albumId, momentId, p.photoUrl, p.title, p.description));
 
     return await this.photoSvc.createMultipleAsync(newPhotos);
+  }
+
+  private async sendNotifications(req: CreateMomentRequest, momentId: number): Promise<void> {
+    const uploader = await this.memberSvc.findByIdAsync(req.creatorMemberId);
+    if (!uploader) {
+      return;
+    }
+
+    const [allMembers, count] = await this.memberSvc.findByAlbumIdAsync(uploader.albumId);
+    allMembers.forEach(async (m) => {
+      const newNotifying = new NotificationEntity(
+        m.userId,
+        NotificationType.AddMember,
+        uploader.userName,
+        uploader.userImageUrl,
+        uploader.albumId,
+        momentId,
+      );
+      await this.notifyingSvc.createAsync(newNotifying);
+    });
   }
 }
